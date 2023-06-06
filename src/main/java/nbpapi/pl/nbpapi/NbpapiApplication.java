27588f45a1810e9a09a3fa643f12f236cd6a7888
mikol <mikolaj.budzinski@onet.pl>
2023-06-06 16:33:58 +0200

    Initial commit

---
+++ src/main/java/nbpapi/pl/nbpapi/NbpapiApplication.java
@@ -0,0 +1,13 @@
+package nbpapi.pl.nbpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbpapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nbpapi/pl/nbpapi/NbpapiApplicationTests.java
@@ -0,0 +1,13 @@
+package nbpapi.pl.nbpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
